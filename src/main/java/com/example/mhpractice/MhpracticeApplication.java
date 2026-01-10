965fb636a75b6536b517d7653f39c495d0ddf858
Minhen96 <evanyap96@gmail.com>
2026-01-10 15:43:39 +0800

    init

---
+++ src/main/java/com/example/mhpractice/MhpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mhpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhpracticeApplication.class, args);
+	}
+
+}
